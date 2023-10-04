853161d49fd0b356ab813e168928960df97103de
Mayur Bhoyar <mayurbhoyar4455@gmail.com>
2023-10-04 17:47:43 +0530

    thirdcommit

--- assests/js/script.js
+++ assests/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "MERN Developer", "Student"],
+    strings: ["frontend developer", "backend developer", "MERN Developer", "Student"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
